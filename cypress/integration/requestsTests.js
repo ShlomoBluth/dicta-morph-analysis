88076f888f30a9c9aac0fb531d0f8650c5196315
Shlomo Bluth <lomibluth@gmail.com>
2021-09-05 11:18:23 +0300

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -4,141 +4,151 @@
 
 ////run tests on requests from citation run some in hebrew mode and english mode
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+//urls.set('live',Cypress.env('LIVE_URL'))
+urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
-sizes.forEach((size) => {
+urls.forEach((urlValue,urlKey)=>{
 
-    describe('RequestsTests',()=>{
+    sizes.forEach((sizeValue,sizeKey) => {
+
     
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'/'})
-        })
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
+    
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
+            })
+
+            // it('Error message for api response with status code 500 when clicking the run button of '+
+            // 'morph analysis page in hebrew mode',()=>{
+            //     cy.morphAnalysisRunRequest({
+            //         url:'/api',
+            //         language:'Hebrew',
+            //         status:500,
+            //         message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+            //     })
+            // })
+    
+            it('Error message for api response with status code 500 when clicking the run button of'+
+            ' morph analysis page in english mode',()=>{
+                cy.morphAnalysisRunRequest({
+                    url:'/api',
+                    language:'English',
+                    status:500,
+                    message:'Oops. Something went wrong Please try again later',
+                })
+            })
+    
+            // it('Error message for api response with a delay of 5 minutes when clicking the run button'+
+            // ' of morph analysis page in hebrew mode',()=>{
+            //     cy.morphAnalysisRunRequest({
+            //         url:'/api',
+            //         language:'Hebrew',
+            //         message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+            //         delaySeconds: 60*5
+            //     })
+            // })
+    
+            it('Error message for api response with a delay of 5 minutes when clicking the run button'+
+            ' of morph analysis page in english mode',()=>{
+                cy.morphAnalysisRunRequest({
+                    url:'/api',
+                    language:'English',
+                    message:'Oops. Something went wrong Please try again later',
+                    delaySeconds: 60*5
+                })
+            })
+        
+            // it('Error message for POST **/files response with status code 500 when clicking the run button '+
+            // 'of citation page in hebrew mode',()=>{
+            //     cy.morphAnalysisUploadRequest({
+            //         language:'Hebrew',
+            //         status:500,
+            //         reqType:'POST',
+            //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
+            //     })    
+            // })
+    
+            it('Error message for POST **/files response with status code 500 when clicking the run button '+
+            'of citation page in English mode',()=>{
+                cy.morphAnalysisUploadRequest({
+                    language:'English',
+                    status:500,
+                    reqType:'POST',
+                    message:'Failed to upload tuvtaamvadaat-014.txt'
+                })    
+            })
+    
+            // it('Error message for POST **/files response with a delay of 1 minutes when'+
+            // ' clicking the run button of citation page in hebrew mode',()=>{
+            //     cy.morphAnalysisUploadRequest({
+            //         language:'Hebrew',
+            //         delaySeconds: 15,
+            //         reqType:'POST',
+            //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
+            //     })    
+            // })
+    
+            it('Error message for POST **/files response with a delay of 1 minutes when'+
+            ' clicking the run button of citation page in hebrew mode',()=>{
+                cy.morphAnalysisUploadRequest({
+                    language:'English',
+                    delaySeconds: 15,
+                    reqType:'POST',
+                    message:'Failed to upload tuvtaamvadaat-014.txt'
+                })    
+            })
+    
+    
+            // it('Error message for PATCH **/files response with status code 500 when clicking the run button '+
+            // 'of citation page in hebrew mode',()=>{
+            //     cy.morphAnalysisUploadRequest({
+            //         language:'Hebrew',
+            //         status:500,
+            //         reqType:'PATCH',
+            //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
+            //     })    
+            // })
+    
+            it('Error message for PATCH **/files response with status code 500 when clicking the run button '+
+            'of citation page in English mode',()=>{
+                cy.morphAnalysisUploadRequest({
+                    language:'English',
+                    status:500,
+                    reqType:'PATCH',
+                    message:'Failed to upload tuvtaamvadaat-014.txt'
+                })    
+            })
+    
+            // it('Error message for PATCH **/files response with status with a delay of 1 minutes when'+
+            // ' clicking the run button of citation page in hebrew mode',()=>{
+            //     cy.morphAnalysisUploadRequest({
+            //         language:'Hebrew',
+            //         delaySeconds: 15,
+            //         reqType:'PATCH',
+            //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
+            //     })    
+            // })
+    
+            it('Error message for PATCH **/files response with status with a delay of 1 minutes when'+
+            ' clicking the run button of citation page in hebrew mode',()=>{
+                cy.morphAnalysisUploadRequest({
+                    language:'English',
+                    delaySeconds: 15,
+                    reqType:'PATCH',
+                    message:'Failed to upload tuvtaamvadaat-014.txt'
+                })    
+            })
+    
       
     
-        // it('Error message for api response with status code 500 when clicking the run button of '+
-        // 'morph analysis page in hebrew mode',()=>{
-        //     cy.morphAnalysisRunRequest({
-        //         url:'/api',
-        //         language:'Hebrew',
-        //         status:500,
-        //         message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        //     })
-        // })
-    
-        it('Error message for api response with status code 500 when clicking the run button of'+
-        ' morph analysis page in english mode',()=>{
-            cy.morphAnalysisRunRequest({
-                url:'/api',
-                language:'English',
-                status:500,
-                message:'Oops. Something went wrong Please try again later',
-            })
-        })
-    
-        // it('Error message for api response with a delay of 5 minutes when clicking the run button'+
-        // ' of morph analysis page in hebrew mode',()=>{
-        //     cy.morphAnalysisRunRequest({
-        //         url:'/api',
-        //         language:'Hebrew',
-        //         message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        //         delaySeconds: 60*5
-        //     })
-        // })
-    
-        it('Error message for api response with a delay of 5 minutes when clicking the run button'+
-        ' of morph analysis page in english mode',()=>{
-            cy.morphAnalysisRunRequest({
-                url:'/api',
-                language:'English',
-                message:'Oops. Something went wrong Please try again later',
-                delaySeconds: 60*5
-            })
-        })
-    
-        // it('Error message for POST **/files response with status code 500 when clicking the run button '+
-        // 'of citation page in hebrew mode',()=>{
-        //     cy.morphAnalysisUploadRequest({
-        //         language:'Hebrew',
-        //         status:500,
-        //         reqType:'POST',
-        //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
-        //     })    
-        // })
-
-        it('Error message for POST **/files response with status code 500 when clicking the run button '+
-        'of citation page in English mode',()=>{
-            cy.morphAnalysisUploadRequest({
-                language:'English',
-                status:500,
-                reqType:'POST',
-                message:'Failed to upload tuvtaamvadaat-014.txt'
-            })    
-        })
-
-        // it('Error message for POST **/files response with a delay of 1 minutes when'+
-        // ' clicking the run button of citation page in hebrew mode',()=>{
-        //     cy.morphAnalysisUploadRequest({
-        //         language:'Hebrew',
-        //         delaySeconds: 15,
-        //         reqType:'POST',
-        //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
-        //     })    
-        // })
-
-        it('Error message for POST **/files response with a delay of 1 minutes when'+
-        ' clicking the run button of citation page in hebrew mode',()=>{
-            cy.morphAnalysisUploadRequest({
-                language:'English',
-                delaySeconds: 15,
-                reqType:'POST',
-                message:'Failed to upload tuvtaamvadaat-014.txt'
-            })    
-        })
-
-
-        // it('Error message for PATCH **/files response with status code 500 when clicking the run button '+
-        // 'of citation page in hebrew mode',()=>{
-        //     cy.morphAnalysisUploadRequest({
-        //         language:'Hebrew',
-        //         status:500,
-        //         reqType:'PATCH',
-        //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
-        //     })    
-        // })
-
-        it('Error message for PATCH **/files response with status code 500 when clicking the run button '+
-        'of citation page in English mode',()=>{
-            cy.morphAnalysisUploadRequest({
-                language:'English',
-                status:500,
-                reqType:'PATCH',
-                message:'Failed to upload tuvtaamvadaat-014.txt'
-            })    
-        })
-
-        // it('Error message for PATCH **/files response with status with a delay of 1 minutes when'+
-        // ' clicking the run button of citation page in hebrew mode',()=>{
-        //     cy.morphAnalysisUploadRequest({
-        //         language:'Hebrew',
-        //         delaySeconds: 15,
-        //         reqType:'PATCH',
-        //         message:'העלאת הקובץ tuvtaamvadaat-014.txt נכשלה '
-        //     })    
-        // })
-
-        it('Error message for PATCH **/files response with status with a delay of 1 minutes when'+
-        ' clicking the run button of citation page in hebrew mode',()=>{
-            cy.morphAnalysisUploadRequest({
-                language:'English',
-                delaySeconds: 15,
-                reqType:'PATCH',
-                message:'Failed to upload tuvtaamvadaat-014.txt'
-            })    
-        })
-    
-        
+        }) 
     })
 })
 
--- cypress/integration/toolTests.js
+++ cypress/integration/toolTests.js
@@ -2,33 +2,44 @@
 
 //run basic tests on citation run
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+//urls.set('live',Cypress.env('LIVE_URL'))
+urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
-sizes.forEach((size) => {
+urls.forEach((urlValue,urlKey)=>{
 
-    describe('toolTests',()=>{
+    sizes.forEach((sizeValue,sizeKey) => {
 
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'/'})
+    
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
+    
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
+            })
+
+            // it('citation run in hebrew mode',()=>{
+            //     cy.setLanguageMode('Hebrew')
+            //     cy.intercept('**/api/markpsukim**').as('req')
+            //     cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
+            //     cy.wait('@req')
+            //     cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
+            // })
+    
+            // it('citation run in english mode',()=>{
+            //     cy.setLanguageMode('English')
+            //     cy.intercept('**/api/markpsukim**').as('req')
+            //     cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
+            //     cy.wait('@req')
+            //     cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
+            // })
+    
         })
-    
-        // it('citation run in hebrew mode',()=>{
-        //     cy.setLanguageMode('Hebrew')
-        //     cy.intercept('**/api/markpsukim**').as('req')
-        //     cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
-        //     cy.wait('@req')
-        //     cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
-        // })
-    
-        // it('citation run in english mode',()=>{
-        //     cy.setLanguageMode('English')
-        //     cy.intercept('**/api/markpsukim**').as('req')
-        //     cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
-        //     cy.wait('@req')
-        //     cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
-        // })
     })
 
 })
